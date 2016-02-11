efc0ff29b2271dd5e4d707839df198c5c5b31cee
Ryan Chenkie <ryanchenkie@gmail.com>
2016-02-11 11:13:57 -0700

    Commented profile.ctr and fixed ui router issues

--- components/profile/profile.ctr.js
+++ components/profile/profile.ctr.js
@@ -15,6 +15,10 @@
     
     vm.profile = JSON.parse(localStorage.getItem('profile'));
     
+    // Makes a call to a public API route that
+    // does not require authentication. We can 
+    // avoid sending the JWT as an Authorization
+    // header with skipAuthorization: true
     function getMessage() {
       $http.get('http://localhost:3001/api/public', {
         skipAuthorization: true 
@@ -23,6 +27,9 @@
       });
     }
     
+    // Makes a call to a private endpoint that does 
+    // require authentication. The JWT is automatically
+    // sent with HTTP calls using jwtInterceptorProvider in app.js
     function getSecretMessage() {
       $http.get('http://localhost:3001/api/private').then(function(response) {
         vm.message = response.data.message;
